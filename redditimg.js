52d2696ac32d3581552a1e601a4fb2946be050da
Stephen Fluin <stephen@mistuph.com>
2010-06-15 22:49:30 -0500

    Added options page and modes

--- redditimg.js
+++ redditimg.js
@@ -1,13 +1,36 @@
-function fixImages(rep) {
+function fix(element) {
+	if(element) {
+		var url = element.href;
+		if(url.substr(-4,4) == ".jpg") {
+			element.innerHTML = '<img src="' + url + '" style="max-width:100%;z-index:4000;"/>';
+			element.style["overflow"] = "inherit";
+			element.style["width"] = "inherit";
+		}
+	} else {
+		console.error("Reddit Imager extension tried to hook up something that doesn't make sense.");
+	}
+}
+function fixImages(rep, now) {
 	var list = $("[href^=http://" + rep + ".com] > img");
 	list = list.parent();
 	for(i = 0;i<list.length;i++) {
-		var url = list[i].href;
-		if(url.substr(-4,4) == ".jpg") {
-			list[i].innerHTML = '<img src="' + url + '" style="max-width:100%;z-index:4000;"/>';
-			list[i].style["overflow"] = "inherit";
-			list[i].style["width"] = "inherit";
+		if(now) {
+			fix(list[i]);
+		} else {
+			list[i].onclick = function() {fix(this);return false;}
 		}
+		
 	}
 	//console.log("Completed fixing images for " + rep + " (found " + list.length + ")");
-}fixImages("imgur");fixImages("i.imgur");
+}
+
+// Check settings through background page
+chrome.extension.sendRequest({action:"check_options"}, function(response) {
+	// Detect whether to activate the event now, or hook it up for later.
+	var now = response.mode != "Activated";
+	fixImages("imgur", now);fixImages("i.imgur", now);
+	
+});
+
+
+
